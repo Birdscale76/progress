3b7f364f37d1661f222157cd078d58b8b39f174f
Birdscale76 <birdscale76@gmail.com>
2025-05-24 22:55:32 +0530

    modified:   app/layout.tsx 	deleted:    app/page.tsx 	deleted:    components/deploy-button.tsx 	deleted:    components/next-logo.tsx 	deleted:    components/supabase-logo.tsx 	deleted:    components/tutorial/code-block.tsx 	deleted:    components/tutorial/connect-supabase-steps.tsx 	deleted:    components/tutorial/fetch-data-steps.tsx 	deleted:    components/tutorial/sign-up-user-steps.tsx 	deleted:    components/tutorial/tutorial-step.tsx 	deleted:    components/typography/inline-code.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
               {children}
             </div>
 
-            <footer id="foot note" className="flex-none w-full border-t border-foreground/10 bg-background h-16">
+            {/* <footer id="foot note" className="flex-none w-full border-t border-foreground/10 bg-background h-16">
               <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between px-5 py-2 h-16">
                 <p className="text-primary text-xs md:text-base text-center">
                   © {new Date().getFullYear()} Drone Progress Pro. All rights reserved.
@@ -68,7 +68,7 @@ export default function RootLayout({
                   <ThemeSwitcher />
                 </div>
               </div>
-            </footer>
+            </footer> */}
           </main>
         </ThemeProvider>
       </body>
